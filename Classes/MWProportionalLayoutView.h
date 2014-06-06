f631523ec187dd0bac1f62492b816b34cc714ea7
Martin Wilz <github@wilz.de>
2014-06-06 22:55:34 +0200

    added docstrings for class

--- Classes/MWProportionalLayoutView.h
+++ Classes/MWProportionalLayoutView.h
@@ -7,9 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+/** a UIView subclass for managing the size and position of subview by
+    a list of given weights. Currently only one hard-codec layout
+    algorithm is available. To use this class use the method
+    setSubviews:withWeights: 
+ */
 
 @interface MWProportionalLayoutView : UIView
 
-- (void)setSubviews:(NSArray*)subviews withWeights:(NSArray *)weights;
+/** set the subviews and their weights to be managed by this view.
+ @param subviews a NSArray of UIView instances
+ @param weights  a NSArray of NSNumbers. must be the same length as subviews. the ordering determines the view for the weight.
+ */
+- (void)setSubviews:(NSArray *)subviews withWeights:(NSArray *)weights;
 
 @end
